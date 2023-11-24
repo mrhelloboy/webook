91a6e6081d9a4af0d4bd93c3b297900d1410febb
mrhelloboy <linzhanqi123@gmail.com>
2023-11-24 16:02:47 +0800

    feat(user): 添加跨域处理

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
 package main
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/mrhelloboy/wehook/web"
+	"strings"
+	"time"
 )
 
 func main() {
 	server := gin.Default()
+
+	server.Use(cors.New(cors.Config{
+		AllowHeaders:     []string{"Content-Type", "Authorization"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			// 开发环境允许跨域
+			if strings.HasPrefix(origin, "http://localhost") {
+				return true
+			}
+			return strings.Contains(origin, "your-company.com")
+		},
+		MaxAge: 1 * time.Hour,
+	}))
+
 	userHandler := web.NewUserHandler()
 	userHandler.RegisterRouters(server)
 	err := server.Run(":8080")
