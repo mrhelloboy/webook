1ea9355e9fc32f124e0ff824e10249ae0587c808
mrhelloboy <linzhanqi123@gmail.com>
2024-03-15 20:20:14 +0800

    feat: 添加解析请求头或者 token 的包装函数

---
+++ pkg/ginx/wrapper.go
@@ -0,0 +1,93 @@
+package ginx
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/mrhelloboy/wehook/pkg/logger"
+)
+
+var L logger.Logger
+
+// WrapToken 包装处理函数, 自动解析token
+func WrapToken[C jwt.Claims](fn func(ctx *gin.Context, uc C) (Result, error)) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		val, ok := ctx.Get("claims")
+		if !ok {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		uc, ok := val.(C)
+		if !ok {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		res, err := fn(ctx, uc)
+		if err != nil {
+			L.Error("处理业务逻辑出错",
+				logger.String("path", ctx.Request.URL.Path),
+				logger.String("route", ctx.FullPath()),
+				logger.Error(err),
+			)
+		}
+		ctx.JSON(http.StatusOK, res)
+	}
+}
+
+// WrapBodyAndToken 包装处理函数, 自动解析请求体, 并解析token
+func WrapBodyAndToken[Req any, C jwt.Claims](fn func(ctx *gin.Context, req Req, uc C) (Result, error)) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req Req
+		if err := ctx.Bind(&req); err != nil {
+			return
+		}
+
+		val, ok := ctx.Get("claims")
+		if !ok {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		uc, ok := val.(C)
+		if !ok {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+		res, err := fn(ctx, req, uc)
+		if err != nil {
+			L.Error("处理业务逻辑出错",
+				logger.String("path", ctx.Request.URL.Path),
+				logger.String("route", ctx.FullPath()),
+				logger.Error(err),
+			)
+		}
+		ctx.JSON(http.StatusOK, res)
+	}
+}
+
+// WrapBody 包装处理函数, 自动解析请求体
+func WrapBody[T any](fn func(ctx *gin.Context, req T) (Result, error)) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		var req T
+		if err := ctx.Bind(&req); err != nil {
+			return
+		}
+
+		res, err := fn(ctx, req)
+		if err != nil {
+			L.Error("处理业务逻辑出错",
+				logger.String("path", ctx.Request.URL.Path),
+				logger.String("route", ctx.FullPath()),
+				logger.Error(err),
+			)
+		}
+		ctx.JSON(http.StatusOK, res)
+	}
+}
+
+type Result struct {
+	// 这个叫做业务错误码
+	Code int    `json:"code"`
+	Msg  string `json:"msg"`
+	Data any    `json:"data"`
+}
